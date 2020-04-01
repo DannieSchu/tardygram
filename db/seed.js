b5847be88a585e53e6e656a6e3d46fcd2b7d91e4
Dannie Schumaker <draeschumaker@gmail.com>
2020-04-01 15:47:14 -0700

    seed database with random comments

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,9 @@
 const chance = require('chance').Chance();
 const User = require('../lib/models/User');
 const Post = require('../lib/models/Post');
+const Comment = require('../lib/models/Comment');
 
-module.exports = async({ usersToCreate = 5, postsToCreate = 20 } = {}) => {
+module.exports = async({ usersToCreate = 5, postsToCreate = 20, commentsToCreate = 20 } = {}) => {
   const loggedInUser = await User.create({
     username: 'gnome',
     password: 'feelinggnomey',
@@ -17,10 +18,16 @@ module.exports = async({ usersToCreate = 5, postsToCreate = 20 } = {}) => {
     profilePhotoURL: chance.avatar({ fileExtension: 'jpg' })
   })));
 
-  await Post.create([...Array(postsToCreate)].map(() => ({
+  const posts = await Post.create([...Array(postsToCreate)].map(() => ({
     user: chance.weighted([loggedInUser, ...users], [2, ...users.map(() => 1)])._id,
     photoURL: chance.avatar({ fileExtension: 'jpg' }),
     caption: chance.sentence(),
     tags: chance.pickset(tagOptions, 3)
+  })));
+
+  await Comment.create([...Array(commentsToCreate)].map(() => ({
+    commentBy: chance.weighted([loggedInUser, ...users], [2, ...users.map(() => 1)])._id,
+    post: chance.pickone(posts)._id,
+    comment: chance.sentence()
   })));
 };
