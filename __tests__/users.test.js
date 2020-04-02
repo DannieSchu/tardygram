3c942045da0e22b6a09e90f1bb3650e170d80b5f
Dannie Schumaker <draeschumaker@gmail.com>
2020-04-01 21:48:48 -0700

    modify mostProlific test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,13 +1,14 @@
 const request = require('supertest');
 const app = require('../lib/app');
+require('../db/data-helpers');
 
 describe('users routes', () => {
   it('finds most prolific posters', () => {
     return request(app)
       .get('/api/v1/users/prolific')
       .then(res => {
-        expect(res.body.length).toContainEqual(10);
-        expect(res.body).toEqual({
+        expect(res.body.length).toEqual(10);
+        expect(res.body).toContainEqual({
           _id: expect.any(String),
           username: expect.any(String),
           totalPosts: expect.any(Number)
